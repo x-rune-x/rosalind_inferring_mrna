16189f3ee8f881a80684f1499adf79e8f0c9116a
James Bedford <jamesbedford@protonmail.com>
2021-02-23 13:57:50 +1100

    Added comments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Map amino acid by letter code to the number of codons that code for it.
 codon_count = {
     "F": 2,
     "L": 6,
@@ -25,14 +26,16 @@ codon_count = {
 def calc_rna(input_text, codon_dict):
     input_file = open(input_text, "r")
 
+    # Begin with 3 to take into account the 3 stop codons that will always be present in an mRNA sequence.
     total_combinations = 3
 
     rna_string = input_file.read().strip("\n")
+    # Multiply each amino acid in the sequence by its number of codons to get the total mRNA combinations.
     for aa in rna_string:
         total_combinations = total_combinations * codon_dict[aa]
+        # The problem asks to provide the answer to modulo 1000000 to avoid dealing with very large numbers.
         total_combinations = total_combinations % 1000000
 
-    # total_combinations = total_combinations % 1000000
     return total_combinations
 
 
